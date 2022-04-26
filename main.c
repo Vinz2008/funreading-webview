39aa42dcfc92e5cd341e054ee3d310ae1fd16d75
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-04-26 18:24:55 +0200

    removed useless function

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 #define WEBVIEW_HEADER
 #include "webview.h"
 
-/*void myFunc(const char *seq, const char *req, void *arg) {
-	printf("Params: %s\n", req);
-}*/
 
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
@@ -17,7 +14,6 @@ int main() {
 	webview_t w = webview_create(0, NULL);
 	webview_set_title(w, "Funreading");
 	webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-	//webview_bind(w, "myFunc", myFunc, NULL);
 	webview_navigate(w, "https://www.funreading.fr");
 	webview_run(w);
 	webview_destroy(w);
